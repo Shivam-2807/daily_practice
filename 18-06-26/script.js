288592d74f70b906443653cdf109092bdd8ea3ec
shivam saini <shivamsaini28.07.04@gmail.com>
2026-06-18 16:04:43 +0530

    add funcnality

--- 18-06-26/script.js
+++ 18-06-26/script.js
@@ -1,8 +1,17 @@
-const darkmode =document.querySelector('#darkmode');
+const darkmode = document.querySelector('#darkmode');
+const asid = document.querySelector('.asid');
 const hero = document.querySelector('.hero');
-const aside = document.querySelector('aside');
+const b1 =document.querySelector('#b1')
+const form1=document.querySelector('.form1');
+const closebtn=document.querySelector('#closebtn')
 
 darkmode.addEventListener('click',()=>{
+    asid.classList.toggle('dark-active')
     hero.classList.toggle('dark-active')
-    aside.classList.toggle('dark-active')
+});
+b1.addEventListener('click', () =>{
+   form1.style.display = 'flex';
+});
+closebtn.addEventListener('click',() =>{
+    form1.style.display='none';
 });
